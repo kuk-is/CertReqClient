22ed5468677c7534e5e8d3c5b0061a17efdb1a3c
Filipe Ramos <fra@kuk-is.de>
2019-07-01 13:24:48 +0200

    created the GUI

---
+++ CertReqClient/CertReqClient/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace CertReqClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label_common_name = new System.Windows.Forms.Label();
+            this.textBox_commonName = new System.Windows.Forms.TextBox();
+            this.textbox_alternative_names = new System.Windows.Forms.Label();
+            this.textbox_alternativeNames = new System.Windows.Forms.TextBox();
+            this.label_organization = new System.Windows.Forms.Label();
+            this.textbox_organization = new System.Windows.Forms.TextBox();
+            this.Department = new System.Windows.Forms.Label();
+            this.textBox_Department = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_City = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox_State = new System.Windows.Forms.TextBox();
+            this.textBox_Country = new System.Windows.Forms.TextBox();
+            this.textBox_Keysize = new System.Windows.Forms.TextBox();
+            this.btn_generate = new System.Windows.Forms.Button();
+            this.btn_cancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label_common_name
+            // 
+            this.label_common_name.AutoSize = true;
+            this.label_common_name.Location = new System.Drawing.Point(81, 108);
+            this.label_common_name.Name = "label_common_name";
+            this.label_common_name.Size = new System.Drawing.Size(82, 13);
+            this.label_common_name.TabIndex = 0;
+            this.label_common_name.Text = "Common Name:";
+            // 
+            // textBox_commonName
+            // 
+            this.textBox_commonName.Location = new System.Drawing.Point(185, 105);
+            this.textBox_commonName.Multiline = true;
+            this.textBox_commonName.Name = "textBox_commonName";
+            this.textBox_commonName.Size = new System.Drawing.Size(217, 26);
+            this.textBox_commonName.TabIndex = 1;
+            // 
+            // textbox_alternative_names
+            // 
+            this.textbox_alternative_names.AutoSize = true;
+            this.textbox_alternative_names.Location = new System.Drawing.Point(28, 182);
+            this.textbox_alternative_names.Name = "textbox_alternative_names";
+            this.textbox_alternative_names.Size = new System.Drawing.Size(135, 13);
+            this.textbox_alternative_names.TabIndex = 2;
+            this.textbox_alternative_names.Text = "Subject Alternative Names:";
+            this.textbox_alternative_names.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textbox_alternativeNames
+            // 
+            this.textbox_alternativeNames.Location = new System.Drawing.Point(185, 146);
+            this.textbox_alternativeNames.Multiline = true;
+            this.textbox_alternativeNames.Name = "textbox_alternativeNames";
+            this.textbox_alternativeNames.Size = new System.Drawing.Size(217, 92);
+            this.textbox_alternativeNames.TabIndex = 4;
+            // 
+            // label_organization
+            // 
+            this.label_organization.AutoSize = true;
+            this.label_organization.Location = new System.Drawing.Point(94, 253);
+            this.label_organization.Name = "label_organization";
+            this.label_organization.Size = new System.Drawing.Size(69, 13);
+            this.label_organization.TabIndex = 5;
+            this.label_organization.Text = "Organization:";
+            // 
+            // textbox_organization
+            // 
+            this.textbox_organization.Location = new System.Drawing.Point(185, 250);
+            this.textbox_organization.Multiline = true;
+            this.textbox_organization.Name = "textbox_organization";
+            this.textbox_organization.Size = new System.Drawing.Size(217, 26);
+            this.textbox_organization.TabIndex = 6;
+            // 
+            // Department
+            // 
+            this.Department.AutoSize = true;
+            this.Department.Location = new System.Drawing.Point(98, 294);
+            this.Department.Name = "Department";
+            this.Department.Size = new System.Drawing.Size(65, 13);
+            this.Department.TabIndex = 7;
+            this.Department.Text = "Department:";
+            // 
+            // textBox_Department
+            // 
+            this.textBox_Department.Location = new System.Drawing.Point(185, 291);
+            this.textBox_Department.Multiline = true;
+            this.textBox_Department.Name = "textBox_Department";
+            this.textBox_Department.Size = new System.Drawing.Size(217, 26);
+            this.textBox_Department.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(136, 335);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "City:";
+            // 
+            // textBox_City
+            // 
+            this.textBox_City.Location = new System.Drawing.Point(185, 332);
+            this.textBox_City.Multiline = true;
+            this.textBox_City.Name = "textBox_City";
+            this.textBox_City.Size = new System.Drawing.Size(217, 26);
+            this.textBox_City.TabIndex = 10;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(128, 374);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "State:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(117, 415);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Country:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(117, 457);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Keysize:";
+            // 
+            // textBox_State
+            // 
+            this.textBox_State.Location = new System.Drawing.Point(185, 371);
+            this.textBox_State.Multiline = true;
+            this.textBox_State.Name = "textBox_State";
+            this.textBox_State.Size = new System.Drawing.Size(217, 26);
+            this.textBox_State.TabIndex = 14;
+            // 
+            // textBox_Country
+            // 
+            this.textBox_Country.Location = new System.Drawing.Point(185, 412);
+            this.textBox_Country.Multiline = true;
+            this.textBox_Country.Name = "textBox_Country";
+            this.textBox_Country.Size = new System.Drawing.Size(217, 26);
+            this.textBox_Country.TabIndex = 15;
+            // 
+            // textBox_Keysize
+            // 
+            this.textBox_Keysize.Location = new System.Drawing.Point(185, 454);
+            this.textBox_Keysize.Multiline = true;
+            this.textBox_Keysize.Name = "textBox_Keysize";
+            this.textBox_Keysize.Size = new System.Drawing.Size(217, 26);
+            this.textBox_Keysize.TabIndex = 16;
+            // 
+            // btn_generate
+            // 
+            this.btn_generate.Location = new System.Drawing.Point(185, 505);
+            this.btn_generate.Name = "btn_generate";
+            this.btn_generate.Size = new System.Drawing.Size(93, 31);
+            this.btn_generate.TabIndex = 17;
+            this.btn_generate.Text = "Generate";
+            this.btn_generate.UseVisualStyleBackColor = true;
+            this.btn_generate.Click += new System.EventHandler(this.btn_generate_Click);
+            // 
+            // btn_cancel
+            // 
+            this.btn_cancel.Location = new System.Drawing.Point(309, 505);
+            this.btn_cancel.Name = "btn_cancel";
+            this.btn_cancel.Size = new System.Drawing.Size(93, 31);
+            this.btn_cancel.TabIndex = 18;
+            this.btn_cancel.Text = "Cancel";
+            this.btn_cancel.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(518, 613);
+            this.Controls.Add(this.btn_cancel);
+            this.Controls.Add(this.btn_generate);
+            this.Controls.Add(this.textBox_Keysize);
+            this.Controls.Add(this.textBox_Country);
+            this.Controls.Add(this.textBox_State);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox_City);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_Department);
+            this.Controls.Add(this.Department);
+            this.Controls.Add(this.textbox_organization);
+            this.Controls.Add(this.label_organization);
+            this.Controls.Add(this.textbox_alternativeNames);
+            this.Controls.Add(this.textbox_alternative_names);
+            this.Controls.Add(this.textBox_commonName);
+            this.Controls.Add(this.label_common_name);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label_common_name;
+        private System.Windows.Forms.TextBox textBox_commonName;
+        private System.Windows.Forms.Label textbox_alternative_names;
+        private System.Windows.Forms.TextBox textbox_alternativeNames;
+        private System.Windows.Forms.Label label_organization;
+        private System.Windows.Forms.TextBox textbox_organization;
+        private System.Windows.Forms.Label Department;
+        private System.Windows.Forms.TextBox textBox_Department;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_City;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox_State;
+        private System.Windows.Forms.TextBox textBox_Country;
+        private System.Windows.Forms.TextBox textBox_Keysize;
+        private System.Windows.Forms.Button btn_generate;
+        private System.Windows.Forms.Button btn_cancel;
+    }
+}
+
---
+++ CertReqClient/CertReqClient/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CertReqClient
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_generate_Click(object sender, EventArgs e)
+        {
+            string commonName = textBox_commonName.Text;
+            string SubjectAlternativeNames = textbox_alternativeNames.Text;
+            string Organization = textbox_organization.Text;
+            string Department = textBox_Department.Text;
+            string City = textBox_City.Text;
+            string State = textBox_State.Text;
+            string Country = textBox_Country.Text;
+            string Keysize = textBox_Keysize.Text;
+
+
+        }
+    }
+}
