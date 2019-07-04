8f67cf2e985eaa8d98c40fc1771610242407ba77
Filipe Ramos <fra@kuk-is.de>
2019-07-04 09:57:51 +0200

    created CSR text file and saved to a local folder

--- CertReqClient/CertReqClient/Form1.cs
+++ CertReqClient/CertReqClient/Form1.cs
@@ -1,18 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
+﻿using Org.BouncyCastle.Asn1.X509;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Generators;
+using Org.BouncyCastle.Crypto.Prng;
+using Org.BouncyCastle.OpenSsl;
+using Org.BouncyCastle.Pkcs;
+using Org.BouncyCastle.Security;
+using System;
+using System.IO;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace CertReqClient
 {
     public partial class Form1 : Form
     {
-        
+       
         public Form1()
         {
             InitializeComponent();
@@ -40,7 +42,42 @@ namespace CertReqClient
             string Country = textBox_Country.Text;
             string Keysize = textBox_Keysize.Text;
 
+            
+            var subjectName = "CN=www.copanyName.com,O=Company Name,OU=Department,T=Area,ST=State,C=Country";
 
+            // create new Object for Issuer and Subject
+            var issuer = new X509Name(subjectName);
+            var subject = new X509Name(subjectName);
+
+            // generate the key Value Pair, which in our case is a public Key
+            var randomGenerator = new CryptoApiRandomGenerator();
+            var random = new SecureRandom(randomGenerator);
+            AsymmetricCipherKeyPair subjectKeyPair = default(AsymmetricCipherKeyPair);
+            const int strength = 2048;
+            var keyGenerationParameters = new KeyGenerationParameters(random, strength);
+
+            var keyPairGenerator = new RsaKeyPairGenerator();
+            keyPairGenerator.Init(keyGenerationParameters);
+            subjectKeyPair = keyPairGenerator.GenerateKeyPair();
+            AsymmetricCipherKeyPair issuerKeyPair = subjectKeyPair;
+
+            // PKCS #10 Certificate Signing Request
+            Pkcs10CertificationRequest csr = new Pkcs10CertificationRequest("SHA1WITHRSA", subject, issuerKeyPair.Public, null, issuerKeyPair.Private);
+
+            // convert BouncyCastle CSR to .PEM file.
+            StringBuilder CSRPem = new StringBuilder();
+            PemWriter CSRPemWriter = new PemWriter(new StringWriter(CSRPem));
+            CSRPemWriter.WriteObject(csr);
+            CSRPemWriter.Writer.Flush();
+
+            // get CSR text
+            var CSRtext = CSRPem.ToString();
+
+            // write content into text file
+            using (StreamWriter f = new StreamWriter(@"C:\Cert_TEST\DemoCSR.txt"))
+            {
+                f.Write(CSRtext);
+            }
         }
     }
 }
--- CertReqClient/CertReqClient/main.cs
+++ CertReqClient/CertReqClient/main.cs
@@ -8,6 +8,40 @@ namespace CertReqClient
 {
     class main
     {
-        
+        public static string PemEncodeSigningRequest(CertificateRequest request, PkcsSignatureGenerator generator)
+        {
+            byte[] pkcs10 = request.CreateSigningRequest(generator);
+            StringBuilder builder = new StringBuilder();
+
+            builder.AppendLine("-----BEGIN CERTIFICATE REQUEST-----");
+
+            string base64 = Convert.ToBase64String(pkcs10);
+
+            int offset = 0;
+            const int LineLength = 64;
+
+            while (offset < base64.Length)
+            {
+                int lineEnd = Math.Min(offset + LineLength, base64.Length);
+                builder.AppendLine(base64.Substring(offset, lineEnd - offset));
+                offset = lineEnd;
+            }
+
+            builder.AppendLine("-----END CERTIFICATE REQUEST-----");
+            return builder.ToString();
+        }
+
+    }
+
+    public class PkcsSignatureGenerator
+    {
+    }
+
+    public class CertificateRequest
+    {
+        internal byte[] CreateSigningRequest(PkcsSignatureGenerator generator)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
